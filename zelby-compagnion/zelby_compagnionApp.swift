a85ddd64d3039b85491473941ec5b162fe2cae0b
Maxime <diasmaxime1@gmail.com>
2024-07-11 14:50:27 +0200

    Initial Commit

---
+++ zelby-compagnion/zelby_compagnionApp.swift
@@ -0,0 +1,17 @@
+//
+//  zelby_compagnionApp.swift
+//  zelby-compagnion
+//
+//  Created by Maxime Dias on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct zelby_compagnionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
